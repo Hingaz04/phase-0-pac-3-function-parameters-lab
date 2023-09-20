0d5bcf27fdcdaf010be4ccf500a9ccfcfe5fc07e
Hingaz <hhinga04@gmail.com>
2023-09-20 13:56:16 +0300

    Complete assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function introduction(Name){
+  console.log("Hi, my name is ${name}");
+
+}
+
+
+function introductionWithLanguage(name, language){
+  console.log("Hi, my name is ${name} and I am learning to program in ${language}");
+}
+
+function introductionWithLanguageOptional(name, language="Javascript"){
+  console.log("Hi my name is ${name} and I am learning to program in ${language}");
+}
